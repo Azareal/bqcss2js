eb9b6525ed4b89bd9ee8459844087b88e8219646
Azareal <azareal@users.noreply.github.com>
2018-08-21 09:42:18 +1000

    Slightly late and still non-functional and experimental.

---
+++ parse/config.go
@@ -0,0 +1,5 @@
+package parse
+
+type Config struct {
+	Autostart bool // Will the script automatically run against the page or does it need to be called by another script first?
+}
--- parse/gen.go
+++ parse/gen.go
@@ -5,9 +5,34 @@ import (
 	"strconv"
 )
 
-// TODO: Strip the newlines with an init() function to reduce the amount of data sent over the wire?
-// TODO: Allow some scripts to skip attachObserver, if they opt for manual calls to widthChanged instead
-var jsPrefix = []byte(`"use strict"
+// TODO: Add support for styling the immediate element not just the descendents
+// TODO: Is ResizeObserver a good fallback for MutationObserver?
+// ? - Can we use V8 / Node / Otto for testing the generated javascript? Maybe spin it up in the browser or something?
+func Output(queries []*Query, config *Config) (cssBytes []byte, jsBytes []byte, err error) {
+	// TODO: Strip the newlines with an init() function to reduce the amount of data sent over the wire?
+	// TODO: Allow some scripts to skip attachObserver, if they opt for manual calls to widthChanged instead
+	// TODO: Vary the output based on which rules are used, e.g. don't output widthChanged, if there's no width related queries
+	// TODO: Use a class instead of loose JS functions
+	var jsPrefix = `"use strict"
+const RuleTypes = Object.freeze({"minWidth":0, "maxWidth":1});`
+	// TODO: Run the autostart function on document.ready?
+	if config.Autostart {
+		jsPrefix += `
+(() => {
+	initBqcss();
+})();`
+	}
+	jsPrefix += `
+function initBqcss() {
+	Object.keys(selToStyleID).forEach((key) => {
+		let elements = document.querySelectorAll(key)
+		for(var i = 0; i < elements.length; i++) {
+			elemMappings[elements[i]] = {
+				"styleID": selToStyleID[key],
+			};
+		}
+	});
+}
 function widthChanged(element) {
 	//
 }
@@ -21,23 +46,16 @@ function attachObserver(element) {
 	});
 	observer.observe(element, {attributes:true, childList: true, subtree: true});
 }
-let elemsToStyleIDs = {};
-let selToStyleID = {
-`)
-
-// TODO: Add support for styling the immediate element not just the descendents
-// TODO: Is ResizeObserver a good fallback for MutationObserver?
-// ? - Can we use V8 / Node / Otto for testing the generated javascript? Maybe spin it up in the browser or something?
-func Output(queries []*Query) (cssBytes []byte, jsBytes []byte, err error) {
-	seed := make([]byte, len(jsPrefix))
-	copy(seed, jsPrefix)
-	jsBuf := bytes.NewBuffer(seed)
+var elemMappings = {};
+var selToStyleID = {
+`
+	jsBuf := bytes.NewBuffer([]byte(jsPrefix))
 	cssBuf := bytes.NewBuffer(nil)
 	for i, query := range queries {
-		cssBuf.WriteString("[fairy-dust-" + strconv.Itoa(i) + "]{")
+		cssBuf.WriteString("[bqcss-" + strconv.Itoa(i) + "]{")
 		cssBuf.WriteString(query.Body)
 		cssBuf.Write([]byte("}\n"))
-		jsBuf.Write([]byte(`"` + query.Selector + `"` + ":" + strconv.Itoa(i)))
+		jsBuf.WriteString(`"` + query.Selector + `"` + ":" + strconv.Itoa(i) + ",")
 	}
 	jsBuf.Write([]byte("};"))
 	return cssBuf.Bytes(), jsBuf.Bytes(), nil
